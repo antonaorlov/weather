66e7ef9022efe6d5c17d59189a20804d554b837b
Antonorlov <66281524+antonaorlov@users.noreply.github.com>
2023-11-22 22:33:14 -0500

    Add files via upload

---
+++ weather/weather/views/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingView.swift
+//  weather
+//
+//  Created by user243761 on 11/19/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
---
+++ weather/weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by user243761 on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
